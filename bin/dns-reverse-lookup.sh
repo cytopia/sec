e1955a2f840e40228d6a7042be479a4d77990406
cytopia <cytopia@everythingcli.org>
2019-03-22 23:40:34 +0100

    Scan DNS in parallel

--- bin/dns-reverse-lookup.sh
+++ bin/dns-reverse-lookup.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 print_usage() {
-	echo "$0 <file-ips> <file-dns> <file-output>"
+	echo "$0 <file-ips> <file-dns>"
 	echo
 	echo "Tries to UDP-based reverse dns lookup every IP address against every DNS server"
 	echo "via the host command."
@@ -10,31 +10,49 @@ print_usage() {
 	echo "                 One IP address per line."
 	echo "<file-dns>       Path to file which contains IP addresses of DNS servers to use for lookup."
 	echo "                 One IP address per line."
-	echo "<file-output>    Location to store the results"
 }
 
-if [ "${#}" -ne "3" ]; then
+get_ptr() {
+	local target="${1}"
+	local dns_server="${2}"
+
+	# temporary
+	local PTR=
+	local addr=
+	local name=
+
+	if PTR="$( host -R 1 -W 1 -t PTR "${target}" "${dns_server}" )"; then
+		if PTR="$( echo "${PTR}" | grep 'in-addr.arpa' )"; then
+			addr="$( echo "${PTR}" | sed 's/in-addr.*$//g' | tac -s. | xargs | sed 's/\.$//g' )"
+			name="$( echo "${PTR}" | awk '{print $NF}' | sed 's/\.$//g' )"
+			echo "Host: ${target} DNS: ${dns_server} PTR: ${addr} ${name}"
+			return 0
+		fi
+	fi
+	echo "Host: ${target} DNS: ${dns_server}"
+}
+
+if [ "${#}" -ne "2" ]; then
+	>&2 echo "Error, requires exactly two parameters"
 	print_usage
 	exit 1
 fi
-if [ ! -f "${1}" ]; then
-	print_usage
-	exit 1
-fi
+#if [ ! -f "${1}" ]; then
+#       >&2 echo "Error, ${1} is not a file"
+#       print_usage
+#       exit 1
+#fi
 if [ ! -f "${2}" ]; then
+	>&2 echo "Error, ${2} is not a file"
 	print_usage
 	exit 1
 fi
 
 for target in $( cat "${1}" ); do
 	for dns in $( cat "${2}" ); do
-		echo "\$ host -R 1 -W 1 -t PTR ${target} ${dns}"
-		echo "\$ host -R 1 -W 1 -t PTR ${target} ${dns}" >> "${3}"
-		if PTR="$( host -R 1 -W 1 -t PTR "${target}" "${dns}" | grep 'in-addr.arpa' )"; then
-			addr="$( echo "${PTR}" | sed 's/in-addr.*$//g' | tac -s. | xargs | sed 's/\.$//g' )"
-			name="$( echo "${PTR}" | awk '{print $NF}' | sed 's/\.$//g' )"
-			echo "${addr} ${name}"
-			echo "${addr} ${name}" >> "${3}"
-		fi
+		get_ptr "${target}" "${dns}" &
 	done
 done
+
+# Wait for all background tasks to finish
+wait
